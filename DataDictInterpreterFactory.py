f8fde2a5db61d03faf6e5c90743d77a1201d2ec4
kyle <kluoma@ucsd.edu>
2023-09-27 13:20:52 -0700

    Added data dict interpreter factory

---
+++ DataDictInterpreterFactory.py
@@ -0,0 +1,15 @@
+import DataDictInterpreter
+import json
+
+class DataDictInterpreterFactor():
+
+    def __init__(self, database_name: str) -> DataDictInterpreter:
+        db_file_xwalk = json.load(open('./database_dict_info.json', 'r'))
+        data_dict_filename = db_file_xwalk[database_name]
+        filetype = data_dict_filename.split('.')[-1]
+
+        if filetype == "xml":
+            return DataDictInterpreter.XmlDataDictInterpreter(database_name)
+        
+        if filetype == "pdf":
+            return DataDictInterpreter.PdfDataDictInterpreter(database_name)
