0c9107f17f5b2474d1dc38cd926bab6476ac45b5
kyle <kluoma@ucsd.edu>
2023-09-27 13:35:22 -0700

    moved factory class into same file as interpreter

--- DataDictInterpreter.py
+++ DataDictInterpreter.py
@@ -363,3 +363,25 @@ class XmlDataDictInterpreter(DataDictInterpreter):
         for ix, word in enumerate(self.xml_list):
             index[word].append(ix)
         return index
+    
+
+class DataDictInterpreterFactory():
+
+    def __init__(self, database_name: str) -> DataDictInterpreter:
+        self.data_dict_interpreter = self.get_new_interpreter(database_name)
+
+    def get_current_interpreter(self) ->  DataDictInterpreter:
+        return self.data_dict_interpreter
+    
+    def get_new_interpreter(self, database_name: str) -> DataDictInterpreter:
+        db_file_xwalk = json.load(open('./database_dict_info.json', 'r'))
+        data_dict_filename = db_file_xwalk[database_name]
+        filetype = data_dict_filename.split('.')[-1]
+
+        if filetype == "xml":
+            self.data_dict_interpreter =  XmlDataDictInterpreter(database_name)
+            return self.data_dict_interpreter
+        
+        if filetype == "pdf":
+            self.data_dict_interpreter =  PdfDataDictInterpreter(database_name)
+            return self.data_dict_interpreter
